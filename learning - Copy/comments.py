acc76a1a4df60156eded5d1a4d16e0de11a55052
Chinxofb <espicer65@gmail.com>
2025-12-03 07:49:02 +0000

    Add files via upload

---
+++ learning - Copy/comments.py
@@ -0,0 +1,7 @@
+#if you use the hashtag symbol (#) in Python, it indicates that the rest of the line is a comment. Comments are not executed as part of the program; they are meant for human readers to understand the code better.
+# This is a single-line comment in Python
+print("Hello, World!")  # This comment explains that the line prints a greeting message
+"""# Multi-line comments can be created using triple quotes
+"""#line 1 of the comment
+"""#line 2 of the comment
+"""#line 3 of the comment
---
+++ learning - Copy/variables/variables.py
@@ -0,0 +1,22 @@
+#python has not command for declaring 
+#a variable is created the moment you first assign a value to it
+x = 5
+y = "Yellow"
+print(x)
+print(y)
+#variables do not need to be declared with any particular type and can even change type after they have been set
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+#you need to have a worded variable inbetween double or singhle quotes
+print(x)
+#variable names are case-sensitive
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(x)
+print (y)
+print (z)
+#you can get the type of a variable with the type() function
+print(type(x))
+print(type(y))
+print(type(z))
